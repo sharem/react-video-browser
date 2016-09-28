565b3adac31aa1681823cd897ac5fd611b508a4a
Iraide Diaz Orueta <iraide.diaz@gmail.com>
2016-09-28 20:50:12 +0200

    First React component 'SearchBar' created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = process.env.API_KEY;
+
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
