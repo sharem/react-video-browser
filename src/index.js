4d324d8b0b337933a7a359a98bc28834e18582e2
Iraide Diaz Orueta <iraide.diaz@gmail.com>
2016-10-01 18:10:25 +0200

    VideoDetail component added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
-import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = process.env.API_KEY;
 
@@ -20,6 +21,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.videos[0]}/>
 				<VideoList videos={this.state.videos} />
 			</div>
 		);
