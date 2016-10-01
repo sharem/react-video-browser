83820d9dcf3de5d54c1413ac38bc27b49a70ce07
Iraide Diaz Orueta <iraide.diaz@gmail.com>
2016-10-02 00:38:57 +0200

    On video select change the main video functionality added

--- src/index.js
+++ src/index.js
@@ -11,9 +11,11 @@ const API_KEY = process.env.API_KEY;
 class App extends Component {
 	constructor(props) {
 			super(props);
-			this.state = { videos: [] };
+
+			this.state = { videos: [] , selectedVideo: null};
+
 			YTSearch({key: API_KEY, term: 'game'}, (videos) => {
-				this.setState({ videos });
+				this.setState({ videos: videos, selectedVideo: videos[0] });
 			});
 	}
 
@@ -21,8 +23,8 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoDetail video={this.state.videos[0]}/>
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList onVideoSelect={(selectedVideo) => this.setState({selectedVideo})} videos={this.state.videos} />
 			</div>
 		);
 	}
